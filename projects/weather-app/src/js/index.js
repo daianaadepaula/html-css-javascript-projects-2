da972f7ecf273406df19acbd0f2938dcb7eb01aa
Daiana de Paula <daianaadepaula1@gmail.com>
2024-01-03 06:03:47 -0300

    feat: response json api data js

--- projects/weather-app/src/js/index.js
+++ projects/weather-app/src/js/index.js
@@ -6,4 +6,21 @@ const formEl= document.querySelector("form");
 
 formEl.addEventListener("submit", (event)=>{
 	event.preventDefault();
+	const cityValue = cityInputEl.value;
+	getWeatherData(cityValue);
 });
+
+async function getWeatherData(cityValue){
+	try {
+		const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${apikey}&units=metric`);
+
+		if(!response.ok){
+			throw new Error("Network response was not ok")
+		}
+
+		const data = await response.json();
+		console.log(data);
+	} catch (error) {
+		
+	}
+}
