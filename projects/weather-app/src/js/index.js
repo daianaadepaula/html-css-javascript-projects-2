6a59a781004bbc8fe109908461765cf4034cc413
Daiana de Paula <daianaadepaula1@gmail.com>
2024-01-05 07:42:38 -0300

    feat: message error js

--- projects/weather-app/src/js/index.js
+++ projects/weather-app/src/js/index.js
@@ -25,17 +25,25 @@ async function getWeatherData(cityValue){
 		const icon = data.weather[0].icon;
 
 		const details = [
-			`Sensação: ${Math.round(data.main.feels_like)}`,
-			`Umidade: ${data.main.humidity}`,
-			`Vento: ${data.wind.speed}`,
+			`Sensação: ${Math.round(data.main.feels_like)}℃`,
+			`Umidade: ${data.main.humidity}%`,
+			`Vento: ${data.wind.speed} km/h`,
 		];
 
 		weatherDataEl.querySelector(".icon").innerHTML = `<img src="http://openweathermap.org/img/wn/${icon}.png" alt="Ícone do Clima">`;
 		weatherDataEl.querySelector(".temperature").textContent = `${temperature}℃`;
 		weatherDataEl.querySelector(".description").textContent = `${description}`;
 
+		weatherDataEl.querySelector(".details").innerHTML = details
+		.map((detail) => `<div>${detail}</div>`)
+		.join("");
+
 
 	} catch (error) {
-		
+		weatherDataEl.querySelector(".icon").innerHTML = "";
+		weatherDataEl.querySelector(".temperature").textContent = "";
+		weatherDataEl.querySelector(".description").textContent = "Aconteceu um erro, por favor tente novamente mais tarde";
+
+		weatherDataEl.querySelector(".details").innerHTML = "";
 	}
 }
