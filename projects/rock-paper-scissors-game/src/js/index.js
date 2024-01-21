3c468546f25cc2027b9bb30bb8857a120858bf76
Daiana de Paula <daianaadepaula1@gmail.com>
2024-01-21 13:24:04 -0300

    feat: create computerPlay and playRound function

--- projects/rock-paper-scissors-game/src/js/index.js
+++ projects/rock-paper-scissors-game/src/js/index.js
@@ -2,7 +2,27 @@ const buttonsEl = document.querySelectorAll('button');
 
 buttonsEl.forEach(button => {
 	button.addEventListener('click', () => {
-		console.log("You clicked me!", button.id);
+		const result = playRound(button.id, computerPlay());
+		console.log(result);
 	});
 });
 
+function computerPlay(){
+	const choices = ["rock", "paper", "scissors"];
+	const randomChoice = Math.floor(Math.random() * choices.length);
+	return choices[randomChoice];
+}
+
+function playRound(playerSelection, computerSelection){
+	if(playerSelection === computerSelection){
+		return "Empatou!"
+	}else if(
+		(playerSelection === "rock" && computerSelection === "scissors") ||
+		(playerSelection === "paper" && computerSelection === "rock") ||
+		(playerSelection === "scissors" && computerSelection === "paper")
+		) {
+			return "Você Ganhou! " + playerSelection + " bateu " + computerSelection;
+		} else {
+			return "Você perdeu! " + computerSelection + " bateu " + playerSelection;
+		}
+}
